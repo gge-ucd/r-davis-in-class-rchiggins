f021b7efc180c6ead7f3b2461a4adcb3a14cbd85
Conor Higgins <rchiggins@ucdavis.edu>
2019-02-04 19:59:50 -0800

    assignment week 4 update3

--- Script/week_4_assignment_RCH.R
+++ Script/week_4_assignment_RCH.R
@@ -12,3 +12,9 @@ surveys_subset<- surveys[c(1:400), c(1,5:8)]
 
 #step 4
 surveys_subset["hindfoot_length"]
+surveys_subset[surveys_subset$"hindfoot_length">32,]
+
+#This is the values for hindfoot length greater than 32 with NA's ommitted.  If na.omit is not included, NA's will be included
+surveys_long_feet<-na.omit(surveys_subset[surveys_subset$"hindfoot_length">32,])
+surveys_long_feet
+
