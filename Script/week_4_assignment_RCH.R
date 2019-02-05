5305d31538907d16df2e05e2dcec5e687ff9d943
Conor Higgins <rchiggins@ucdavis.edu>
2019-02-04 19:25:21 -0800

    week 4 assignment update2

--- Script/week_4_assignment_RCH.R
+++ Script/week_4_assignment_RCH.R
@@ -1,7 +1,14 @@
 #Week_4_assignment_RCH.R
 
+#step 1
 read.csv("data/portal_data_joined.csv")
 
+#step 2
 surveys <- read.csv(file="data/portal_data_joined.csv")
 
-surveys[1:400,1:8]
+#step 3
+surveys[c(1:400), c(1,5:8)]
+surveys_subset<- surveys[c(1:400), c(1,5:8)]
+
+#step 4
+surveys_subset["hindfoot_length"]
