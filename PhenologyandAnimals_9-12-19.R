6e2c204bfae7b9b53021061f355e2389f037f884
Conor Higgins <rchiggins@ucdavis.edu>
2019-09-12 14:59:52 -0700

    Masters thesis data upload

---
+++ PhenologyandAnimals_9-12-19.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+Phenodata <- read.csv(file = "~/2018-2020 Masters Thesis/Greenland data/Kanger phenology Julian Dates 93-17 SPSS from Eric 9.11.19.csv")
+
+View(Phenodata)
+
+Animaldata <- read.csv(file = "~/2018-2020 Masters Thesis/Greenland data/Kanger Caribou and Muskox counts WITH GENDER AND CALVES 1993 - 2019 Conor 7.29.19 version.csv")
+
+View(Animaldata)
+
+nrow(Animaldata)
+ncol(Animaldata)
+summary(Animaldata)
